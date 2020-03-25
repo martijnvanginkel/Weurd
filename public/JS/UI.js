35164f52587219f82f75ba97b92c8a3a794918d7
Martijn van Ginkel <martijnvanginkel2@gmail.com>
2020-03-25 17:34:25 +0100

    Able to select a list on the index page

--- public/JS/UI.js
+++ public/JS/UI.js
@@ -1,21 +1,40 @@
-document.querySelector('#add_word_btn').addEventListener('click', () => {
-    const parent = document.querySelector('form');
-    const input = document.createElement('div');
-    const save_button = document.querySelector('#save_list_btn');
+if (document.querySelector('#add_word_btn') !== null) {    
+    document.querySelector('#add_word_btn').addEventListener('click', () => {
+        const parent = document.querySelector('form');
+        const input = document.createElement('div');
+        const save_button = document.querySelector('#save_list_btn');
 
-    input.className = 'word_field';
-    input.innerHTML = `
-        <label>langOne</label>
-        <input type="text" name="langOne">
-        <label>langTwo</label>
-        <input type="text" name="langTwo">
-        <button type="button" class="remove_word_btn">X</button>
-    `;
-    input.querySelector('.remove_word_btn').addEventListener('click', e => e.target.parentElement.remove());
-    parent.insertBefore(input, save_button);
-});
+        input.className = 'word_field';
+        input.innerHTML = `
+            <label>langOne</label>
+            <input type="text" name="langOne">
+            <label>langTwo</label>
+            <input type="text" name="langTwo">
+            <button type="button" class="remove_word_btn">X</button>
+        `;
+        input.querySelector('.remove_word_btn').addEventListener('click', e => e.target.parentElement.remove());
+        parent.insertBefore(input, save_button);
+    });
+}
 
 const remove_buttons = document.querySelectorAll('.remove_word_btn')
 for (const button of remove_buttons) {
     button.addEventListener('click', e => e.target.parentElement.remove());
 }
+
+const select_buttons = document.querySelectorAll('.select_item_btn');
+select_buttons.forEach(button => button.addEventListener('click', (e) => {
+    if (document.querySelector('.selected_item') !== null) {
+        const parent = document.querySelector('.selected_item');
+        console.log(parent);
+        const child =  parent.querySelector('.select_item_btn');
+        console.log(child);
+        child.innerHTML = `unselected`;
+       // parent.querySelector('.selected_item_btn').innerHTML = `selected`;
+        // console.log(btn_child);
+        parent.classList.remove('selected_item');
+    }
+    e.target.parentElement.classList.add('selected_item');
+    e.target.innerHTML = `selected`;
+}))
+
